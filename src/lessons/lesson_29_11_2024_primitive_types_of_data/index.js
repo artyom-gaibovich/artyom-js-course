954598528d3bc08beb0656ed2e41056b77ecd8cf
Артём Гайбович <artyomguybov@MacBook-Pro-Artem.local>
2024-12-01 16:51:17 +0300

    primitive data types in the js, 29_11_2024 lesson

---
+++ src/lessons/lesson_29_11_2024_primitive_types_of_data/index.js
@@ -0,0 +1,39 @@
+
+let numberVariable = -1000; //number
+let floatVariable = 1000.300; //number
+
+let bigIntVariable = 999n;
+
+let boolVariable = true;
+
+let strVariable1 = 'It s a some text'
+
+let strVariable2 = "It's a some text"
+
+let firstText = 'This is first line\nThis is second line';
+
+let secondText = `
+This is first line
+This is second line
+`;
+console.log('2313213212132'.toString());
+
+
+let nullVariable = null;
+let undefinedVariable;
+
+let symbolVariable = Symbol('Some symbol');
+
+
+
+console.log('nullVariable', nullVariable);
+console.log('nullVariable', undefinedVariable);
+console.log(`${name} is ${age} years old v1`);
+console.log(name + ' is ' + age + ' years old v2');
+console.log(name,'is', age,'years old v3');
+
+
+console.log(`Тип данных у ${numberVariable} : ${typeof numberVariable}`)
+console.log(`Тип данных у ${floatVariable} : ${typeof floatVariable}`)
+console.log(`Тип данных у ${floatVariable} : ${typeof bigIntVariable}`)
+console.log(`Тип данных у ${boolVariable} : ${typeof boolVariable}`)
