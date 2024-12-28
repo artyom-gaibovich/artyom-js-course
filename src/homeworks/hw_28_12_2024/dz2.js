c87ca77ed0b6301eb35b06c593dde9692b779db7
Artem <aa-200@mail.ru>
2024-12-28 14:26:55 +0100

    Сделал дз но 3 последних задания не понимаю как сделать

---
+++ src/homeworks/hw_28_12_2024/dz2.js
@@ -0,0 +1,8 @@
+//№2
+// Дана строка. Выведите на экран ее предпоследний символ.
+
+let a = "Devil May Cry";
+
+let b = a.length - 2
+
+console.log(a[b])
---
+++ src/homeworks/hw_28_12_2024/dz3.js
@@ -0,0 +1,8 @@
+//№3
+// Дана строка. Выведите на экран ее предпредпоследний символ.
+
+let a = "London Bridge is falling down";
+
+let b = a.length - 3
+
+console.log(a[b])
