be855a38ac952deb15574842cf20f7a709970771
Artem <aa-200@mail.ru>
2025-02-08 14:13:10 +0100

    Сделал дз 14_01_2025

---
+++ src/homeworks/hw_22_01_2025/dz1.js
@@ -0,0 +1,5 @@
+let massElem = [1,2,3]
+for (let i = 0; i < massElem.length; i++) {
+    massElem[i]++
+}
+console.log(massElem)
---
+++ src/homeworks/hw_22_01_2025/dz2.js
@@ -0,0 +1,6 @@
+let emptyMass = []
+let fullMass = [5, 2, 9]
+for (let i = 0; i < 3; i++) {
+    emptyMass[i] = fullMass[i]
+}
+console.log(emptyMass)
