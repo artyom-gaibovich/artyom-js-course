3b4adf15cffe44ede538aff18c3d3df818b5529f
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-03-30 16:54:18 +0300

    дорешали дз и разобрали подробно reduxe

--- src/homeworks/hw_29_03_2025/dz8.js
+++ src/homeworks/hw_29_03_2025/dz8.js
@@ -2,5 +2,13 @@
 Создай объект, где ключи — слова, а значения — их длины (пример: `{ apple: 5, banana: 6 }`). */
 
 let strArr = ["apple", "banana", "cherry", "date"];
-let obj = strArr.reduce
-//не понял как сделать
+let obj = strArr.reduce(function (acc, cur, index) {
+    acc[cur] = cur.length
+    return acc;
+}, {})
+
+
+
+
+
+console.log();
