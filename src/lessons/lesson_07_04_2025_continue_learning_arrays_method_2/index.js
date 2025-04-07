a46411893e48ae94243d0818f3dd18847bc0e92b
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-04-07 19:56:28 +0300

    метод .sort()

---
+++ src/lessons/lesson_07_04_2025_continue_learning_arrays_method_2/index.js
@@ -0,0 +1,19 @@
+/*
+const myArray = ['egor', 'fedos', 'grisha', 'dimka']
+
+const slicedArray = myArray.slice(0,2)
+
+const splicedArray = myArray.splice(0,2)
+
+console.log(slicedArray);*/
+
+
+const myNumbersFirst = [1, 2, 3, 4, 5, 6];
+
+
+
+const result = [].concat(myNumbersFirst)
+result[0] = 999
+console.log(result)
+
+
