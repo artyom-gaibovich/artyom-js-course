123487ec1531f7a9153f07b8640966d58f774d0e
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-05-05 20:15:24 +0300

    разобрали optional chaining

--- src/lessons/lesson_05_05_2025_objects/index.js
+++ src/lessons/lesson_05_05_2025_objects/index.js
@@ -1,3 +1,8 @@
+
+/**
+ * Итерирование по объекту
+ */
+
 const user = {
 	name: 'Vas',
 	age: 15,
@@ -7,6 +12,74 @@ const user = {
 }
 
 
+/*
+1 способ
+for (const key in user) {
+	if (key === 'age') {
+		user[key] += 100;
+	}
+}*/
 
 
-user.greet()
+/**
+ * 2 ой способ
+ * for (const key of Object.keys(user) {
+ *   console.log(user[key])
+ * }
+ */
+
+/**
+ * 3-ой способ объектно ориентированный
+ *
+ * Object.entries(user).forEach(([key, value]) => {
+ * 	console.log(key, value);
+ * })
+ */
+
+/**
+ * Деструктуризация
+ */
+
+let someUser = {
+	name: 'Vas',
+	age: 15,
+	country: 'Russia'
+}
+
+
+const someUserAdditionalProps = {
+	isHasGirlfriend: false,
+	isHasDog: true,
+	car: 'BMW',
+}
+
+
+const newSomeUser = {
+	...someUser,
+	...someUserAdditionalProps
+}
+
+
+/**
+ * Optional Chaining
+ */
+
+
+
+const countries = {
+	russia: {
+		capital: {
+			name: 'Moscow',
+		}
+	},
+	china: {
+
+	}
+}
+
+
+console.log(countries['russia']?.capital?.name)
+
+
+
+
