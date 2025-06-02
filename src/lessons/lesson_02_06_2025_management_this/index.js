7916f34fce13b823b535442294bda27287f21707
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-06-02 20:01:35 +0300

    разобарли пример с потерей контекста

--- src/lessons/lesson_02_06_2025_management_this/index.js
+++ src/lessons/lesson_02_06_2025_management_this/index.js
@@ -1,26 +1,24 @@
 'use strict';
 
-console.log(this)
+console.log(this);
 
 function addFirst(first, second) {
-	console.log(this)
+	console.log(this);
 	return first + second;
 }
-
-
-
 
 
 const addSecond = (first, second) => {
-	console.log(this)
+	console.log(this);
 
-	function addThird () {
-		console.log(this)
+	function addThird() {
+		console.log(this);
 		debugger
 	}
+
 	addThird();
 	return first + second;
-}
+};
 
 
 const user = {
@@ -28,19 +26,19 @@ const user = {
 	email: 'john@example.com',
 	getName: function() {
 		debugger
-		console.log('Hello', this.name)
+		console.log('Hello', this.name);
 	},
 	getNameArrow: () => {
-		console.log(this.name)
-	}
-}
+		console.log(this.name);
+	},
+};
 
 
 const user2 = {
 	name: 'Dima',
 	email: 'dima@example.com',
 
-}
+};
 
 /*
 user.getName()
@@ -55,17 +53,46 @@ user2.getName();
 */
 
 
-
-
-
 const user3 = {
 	name: 'Dima',
 	email: 'dima@example.com',
-}
+};
 
 const newGetName = user.getName;
 
 user3.getName = newGetName;
 
-//newGetName()
-user3.getName()
+/*newGetName()
+user3.getName()*/
+
+//var, let, const, functions, arguments
+//Scope Chain
+//this
+
+
+
+
+function funcValidate(fn) {
+
+	 function validate(...args) {
+		Object.values(arguments).forEach((value) => {
+			if (typeof value !== 'number') {
+				throw new Error(`Value ${value} is not a number`);
+			}
+		})
+	}
+	validate();
+	fn()
+}
+
+
+
+
+function sumFunc(num1, num2) {
+	return num1 + num2;
+}
+
+funcValidate()
+
+
+sumFunc(1, 9);
