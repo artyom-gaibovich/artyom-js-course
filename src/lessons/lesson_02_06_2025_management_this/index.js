b277ce04dafa9000f0f825e835d153ef3b5400fa
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-06-02 19:44:03 +0300

    разобрали пример обычноыми функциями и методами в объекте

--- src/lessons/lesson_02_06_2025_management_this/index.js
+++ src/lessons/lesson_02_06_2025_management_this/index.js
@@ -1,1 +1,50 @@
 'use strict';
+
+console.log(this)
+
+function addFirst(first, second) {
+	console.log(this)
+	return first + second;
+}
+
+
+
+
+
+const addSecond = (first, second) => {
+	console.log(this)
+
+	function addThird () {
+		console.log(this)
+		debugger
+	}
+	addThird();
+	return first + second;
+}
+
+
+const user = {
+	name: 'John',
+	email: 'john@example.com',
+	getName: function() {
+		console.log('Hello', this.name)
+	},
+	getNameArrow: () => {
+		console.log(this.name)
+	}
+}
+
+
+const user2 = {
+	name: 'Dima',
+	email: 'dima@example.com',
+
+}
+
+user.getName()
+user.getNameArrow()
+
+user2.getName = user.getName;
+
+
+user2.getName();
