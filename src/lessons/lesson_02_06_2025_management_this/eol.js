c287679f59b082894dac7a85b89c03c427776d70
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-06-07 15:29:24 +0300

    завершили урок

---
+++ src/lessons/lesson_02_06_2025_management_this/eol.js
@@ -0,0 +1,23 @@
+/**
+ * EOL ДЛЯ функций
+ * @type {string}
+ */
+const first = 'FIRST_FIRST'
+const user = {
+	name : 'GOSHA',
+	getFullName() {
+		console.log(this.name);
+	}
+}
+
+class User {
+	constructor() {
+		this.name = 'Gosha';
+	}
+
+	getFullName() {
+		console.log(this.name);
+	}
+
+}
+
