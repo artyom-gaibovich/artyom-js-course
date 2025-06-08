c8d6c9834c454dd3543915f5efc3f4b85be4c138
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-06-08 16:29:46 +0300

    нужно сделать матрицу

--- src/lessons/lesson_02_06_2025_management_this/eol.js
+++ src/lessons/lesson_02_06_2025_management_this/eol.js
@@ -1,23 +1,4 @@
-/**
- * EOL ДЛЯ функций
- * @type {string}
- */
-const first = 'FIRST_FIRST'
-const user = {
-	name : 'GOSHA',
-	getFullName() {
-		console.log(this.name);
-	}
-}
 
-class User {
-	constructor() {
-		this.name = 'Gosha';
-	}
 
-	getFullName() {
-		console.log(this.name);
-	}
 
-}
 
---
+++ src/lessons/lesson_08_06_2025_bind_call_apply/index.js
@@ -0,0 +1,26 @@
+console.log('hello world')
+
+/**
+ *
+ * @param {number} n
+ * @returns {number[][]}
+ */
+function createMatrix(n){
+
+}
+
+
+/**
+ * n = 3
+ * return [
+ * 		[0,3,6,9, 12, 15],
+ * 		[18,21,24,27, 30, 33],
+ * 		[36,39,42,45, 48, 51],
+ * 	]
+ *
+ * n = 2
+ * return [
+ * 		[0,3,6,9],
+ * 		[12, 15, 18, 21]
+ * 	]
+ */
