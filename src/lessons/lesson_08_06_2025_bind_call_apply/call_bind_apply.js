dcb9a55e7ec4c56a2590b02c97cdcf53b41f03be
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-06-08 17:46:37 +0300

    рассмотрели слушатели событий(onclick, mouseenter, mouseleave)

--- src/lessons/lesson_08_06_2025_bind_call_apply/call_bind_apply.js
+++ src/lessons/lesson_08_06_2025_bind_call_apply/call_bind_apply.js
@@ -9,32 +9,50 @@ const user = {
 		debugger
 		console.log('Hello', this.name);
 	},
-/*
-	increaseCash(someCash) {
-		this.cash += someCash;
-	},*/
 
 	getNameArrow: () => {
 		console.log(this.name);
 	},
+
+	transactions : []
 };
 
-function increaseCash(someCash, msg) {
-	this.cash += someCash;
-	console.log(msg);
-}
 
 const user2 = {
 	cash: 0,
 	name: 'Johnna',
+	transactions : []
 }
 
 
+
+const balanceManager = {
+	addTransaction(cash) {
+		this.transactions.push(cash);
+		console.log('На счет добавлено: ', cash)
+	}
+}
+
+
+
+const addTransactionUser1 = balanceManager.addTransaction.bind(user);
+const addTransactionUser2 = balanceManager.addTransaction.bind(user2);
+
+
+
+addTransactionUser1(999, 1);
+addTransactionUser1(-999);
+
+addTransactionUser2(-10000000);
+
+/*
 increaseCash.call(user, ...[10, 'hello 1'])
 increaseCash.call(user, ...[100, 'hello 2']);
-
 increaseCash.apply(user, [10, 'hello 3'])
 
+
+user;
+user2;*/
 
 user;
 user2;
@@ -44,5 +62,7 @@ debugger
  *
  * bind
  */
+
+
 
 
