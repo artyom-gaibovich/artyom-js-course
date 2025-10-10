a38d6e89768818957e9c2ff2870eeca2b4aa739c
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-10-10 14:54:25 +0300

    JSC-2: Разобрали call, bind, apply и вспоминили про ключевое слово this

--- src/homeworks/2025/9/dz_10_10_2025/dz1.js
+++ src/homeworks/2025/9/dz_10_10_2025/dz1.js
@@ -1,11 +1,12 @@
 const user = {
-    firstName: "John",
-    lastName: "Doe",
-}
+	firstName: 'John',
+	lastName: 'Doe',
+};
 
 
-function formatName(firstName, lastName){
-    return `${firstName} ${lastName}`;
+function formatName(firstName, lastName) {
+	console.log(this) //undefined в use strict режиме
+	return `${firstName} ${lastName}`;
 }
 
 console.log(user.firstName);
@@ -14,9 +15,49 @@ console.log(user.firstName, user.lastName);
 //Дан объект `calculator` с методами `add` и `multiply` и свойством `value`.
 //Напиши код, который заставит методы работать корректно с этим свойством, даже если их вызывать отдельно (используй `bind`).
 const calculator = {
-    value: 10,
-    add(a) { this.value += a; },
-    multiply(b) { this.value *= b; }
+	value: 10,
+	add(a) {
+		this.value += a;
+	},
+	addFunc (a) {
+		console.log(this)
+		const addArrow = () => {
+			console.log(this)
+		}
+		addArrow()
+
+	},
+	addArrow: () => {
+		console.log(this) // undefined
+	},
+	multiply(b) {
+		this.value *= b;
+	},
+};
+
+
+const calculatorTiny = {
+	value: 1,
+
 }
 
-const addFive = calculator.add;// Должно работать addFive(5) -> value становится 15
+
+/**
+ * call
+ * apply
+ *
+ * bind
+ *
+ *
+ * (позволяют задать чему будет равно this при вызове функции)
+ */
+
+const myAdd = calculator.add.bind(calculator);
+myAdd()
+
+
+
+
+calculator.add.call(calculatorTiny, 100)
+calculator.add.apply(calculatorTiny, [100])
+console.log(calculatorTiny);
