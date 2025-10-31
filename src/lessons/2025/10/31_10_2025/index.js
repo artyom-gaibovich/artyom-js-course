2981109a9260b3feb42a65af7f70ee5bce27bfca
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-10-31 15:44:33 +0300

    JSC-3: Fix разобрали примеры с обработчиками событий на клавишы

--- src/lessons/2025/10/31_10_2025/index.js
+++ src/lessons/2025/10/31_10_2025/index.js
@@ -1,4 +1,33 @@
+/**
+ * Типы событий
+ *     click - клик мышью
+ *     input - ввод данных
+ *     keydown, keyup - нажатие/отпускание клавиши
+ *     mouseover, mouseout - наведение/уход курсора
+ */
 
-function main() {
+
+
+function submitForm() {
+	const input = document.querySelector('.input');
+	const panel = document.querySelector('.panel');
+
+	const inputValue = input.value.trim();
+	if (inputValue === '') {
+		return;
+	}
+	panel.innerText = inputValue
+	input.value = ''
+
+}
+
+
+function handleInputChange(e) {
+	if (e.code === 'Enter') {
+		submitForm()
+	}
+	if (e.code === 'Escape') {
+		e.target.value = '';
+	}
 
 }
