a8d0b2f9ecb1044f21a5b2991c7ddb304a9c79a5
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-11-21 15:50:53 +0300

    lesson-21_11_2025: feat разобрали Sets

---
+++ src/lessons/2025/11/21_11_2025_map_and_sets/index.js
@@ -0,0 +1,98 @@
+/*
+Set — это итерируемая коллекция, которая:
+– хранит только уникальные элементы;
+– не имеет ключей, только значения;
+– может содержать строки, числа, boolean, объекты и т. д.;
+– сохраняет порядок вставки.
+ */
+
+const arrSet = new Set(['Russia', 'USA', 'London', 'London']);
+
+// has(value) — проверяет наличие
+const isHasRussia = arrSet.has('Russia');
+const isHasBelgia = arrSet.has('Belgia');
+
+
+// add(value) — добавление
+arrSet.add('Russia');
+arrSet.add('Paris');
+
+
+// delete(value) — удаление
+
+arrSet.delete('USA');
+
+
+// clear(value) – очистка
+arrSet.clear();
+
+/*
+Перебор Set
+
+Set итерируется так же, как массив.
+ */
+
+
+const newCities = new Set(['Russia', 'USA', 'London', 'London']);
+
+
+for (const city of newCities) {
+	console.log(city, 'город');
+}
+
+console.log(arrSet);
+
+
+/*
+Set ⇄ Array: преобразования
+Массив → Set (уникализация)
+ */
+
+const arr = ['a', 'b', 'a', 1];
+const set = new Set(arr); // {"a", "b", size=2}
+
+/*
+Set → массив (через spread ...)
+ */
+
+const uniqueArr = [...set];
+
+
+/**
+ * Уникализация массива в одну строчку
+ */
+
+const uniqueValues = [...new Set(['a', 'a', 'b', 1])];
+
+
+/**
+ * et и объекты
+ *
+ * Важно понимать: Set считает объект уникальным по ссылке, а не по содержимому.
+ */
+
+const obj1 = { a: 1 };
+const obj2 = { a: 1 }
+
+const setObjects = new Set([obj1, obj2]);
+console.log(setObjects);
+
+
+/*
+  Типичные ошибки
+
+  Ошибка:
+   ❌добавлять одинаковые строки и ждать, что Set их дублирует
+   ✔помнить, что Set всегда уберет дубликаты
+
+
+	Ошибка:
+	❌ Ожидать, что одинаковые объекты считаются одинаковыми
+	✔ Сравнение идёт по ссылкам, а не по значению.
+
+	Ошибка:
+	❌ Пытаться обращаться по индексу (set[0]), или по ключу
+	✔ У Set нет индексов.
+
+ */
+
