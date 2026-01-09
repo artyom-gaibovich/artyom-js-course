45e03f5836b76d385723af10c7d94799588abea5
Artem <aa-200@mail.ru>
2026-01-09 17:14:56 +0100

    lesson-09_01_2026: fix решил задания

--- src/lessons/2026/01/09_01_2026/task.js
+++ src/lessons/2026/01/09_01_2026/task.js
@@ -1,9 +1,51 @@
-/*
+/*Сделать класс врага у которого есть здоровье, и метод получения урона*/
+class Enemy{
+    constructor(health){
+        this.health = health;
+    }
+    takeDamage(amount){
+        this.health -= amount;
+        if(this.health < 0){
+            this.health = 0;
+        }
+        console.log(`Enemy health ${this.health}`);
+    }
+}
+/*Сделать класс светого мяча, который имеет урон(сила) и метод нанесения урона*/
+class LightSaber{
+    constructor(dmg){
+        this.dmg = dmg;
+    }
+    attack(enemy){
+        enemy.takeDamage(this.dmg);
+    }
+}
+/*Сделать класс рыцаря, который в 60% случаев не получает*/
+class Knight extends Enemy {
+    constructor(health) {
+        super(health);
 
-	Сделать класс врага у которого есть здоровье, и метод получения урона
+    }
+    takeDamage(amount){
+        const chance = Math.random();
+        if (chance < 0.6){
+            console.log("Knight blocks the incoming damage")
+            return;
+        }
+        this.health -= amount;
+        if(this.health < 0){
+            this.health = 0;
+        }
+        console.log(`Knight health ${this.health}`);
+    }
+}
 
-	Сделать класс светого мяча, который имеет урон(сила) и метод нанесения урона
+const enemy = new Enemy(100);
+const knight = new Knight(100);
+const saber = new LightSaber(20);
 
-	Сделать класс рыцаря, который в 60% случаев не получает
+console.log("Враг атакует!");
+saber.attack(enemy);
 
- */
+console.log("Рыцарь атакует!");
+saber.attack(knight);
