3bab95785a8de68929a0a94b87011e64df5e6297
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2026-01-09 18:16:15 +0300

    lesson-09_01_2026: fix улучшена логика проверки

--- src/homeworks/2025/12/13_12_2025/dz9.js
+++ src/homeworks/2025/12/13_12_2025/dz9.js
@@ -1,12 +1,15 @@
 const b = new Date(2024, 5, 10,23,59)
 const a = new Date(2024, 5, 10,10,30)
 const c = new Date(2024, 5, 11)
+
 function isSameDay(d1, d2){
-    return (
-        d1.getDay() === d2.getDay() &&
-        d1.getMonth() === d2.getMonth() &&
-        d1.getFullYear() === d2.getFullYear()
-    );
+
+    const checks = [d1.getDay() === d2.getDay(),d1.getMonth() === d2.getMonth(), d1.getFullYear() === d2.getFullYear()];
+
+    return checks.every(Boolean)
+
 }
+
+
 console.log(isSameDay(a, b));
 console.log(isSameDay(a, c));
