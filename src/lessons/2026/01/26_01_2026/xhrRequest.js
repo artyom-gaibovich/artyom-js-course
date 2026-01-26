ded64866dadeaa1308822aa0421cec48fd0bf8af
Artem <aa-200@mail.ru>
2026-01-26 13:01:01 +0100

    lesson-26_01_2026: fix решил задание

--- src/lessons/2026/01/26_01_2026/xhrRequest.js
+++ src/lessons/2026/01/26_01_2026/xhrRequest.js
@@ -5,6 +5,10 @@ request.send()
 request.addEventListener("load", function() {
 	const response = this.responseText
 	const parsedJson = JSON.parse(response)
-
+	let count = 0
+	for (let item of parsedJson) {
+		count += item.id;
+	}
+	console.log(count)
 })
 
