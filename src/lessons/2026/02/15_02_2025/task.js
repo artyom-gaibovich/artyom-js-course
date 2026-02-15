0146de1663fac4a2ac22ffb806a6306cc4ced97b
Artem <aa-200@mail.ru>
2026-02-15 14:49:45 +0100

    lesson-15_02_2026: fix решил задание

--- src/lessons/2026/02/15_02_2025/task.js
+++ src/lessons/2026/02/15_02_2025/task.js
@@ -23,24 +23,3 @@
 
 
 
-/*
-var options = {
-	enableHighAccuracy: true,
-	timeout: 5000,
-	maximumAge: 0,
-};
-
-function success(pos) {
-	var crd = pos.coords;
-
-	console.log("Ваше текущее местоположение:");
-	console.log(`Широта: ${crd.latitude}`);
-	console.log(`Долгота: ${crd.longitude}`);
-	console.log(`Плюс-минус ${crd.accuracy} метров.`);
-}
-
-function error(err) {
-	console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-navigator.geolocation.getCurrentPosition(success, error, options);*/
