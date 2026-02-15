8ca0ea0b5e1a3f353fba4c1f5442411c572569ec
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2026-02-15 22:25:50 +0900

    lesson-15_02_2026: feat дал задачу

---
+++ src/lessons/2026/02/15_02_2025/task.js
@@ -0,0 +1,48 @@
+/**
+ * Задание: «Мой город»
+ *
+ * Напиши асинхронную функцию, которая определяет город пользователя.
+ *
+ * Нужно:
+ *
+ * получить геолокацию пользователя через navigator.geolocation.getCurrentPosition
+ *
+ * обернуть получение координат в Promise
+ *
+ * использовать async / await
+ *
+ * по широте и долготе выполнить fetch к API обратного геокодирования
+ *
+ * API: https://api.bigdatacloud.net/data/reverse-geocode-client?latitude={crd.latitude}&longitude={crd.longitude}&localityLanguage=ru
+ *
+ * получить данные и вывести название города в консоль
+ *
+ * обработать возможные ошибки через try / catch
+ */
+
+
+
+
+
+/**
+ * const options = {
+ *   enableHighAccuracy: true,
+ *   timeout: 25000,
+ *   maximumAge: 0,
+ * };
+ *
+ * function success(pos) {
+ *   var crd = pos.coords;
+ *
+ *   console.log("Ваше текущее местоположение:");
+ *   console.log(`Широта: ${crd.latitude}`);
+ *   console.log(`Долгота: ${crd.longitude}`);
+ *   console.log(`Плюс-минус ${crd.accuracy} метров.`);
+ * }
+ *
+ * function error(err) {
+ *   console.warn(`ERROR(${err.code}): ${err.message}`);
+ * }
+ *
+ * navigator.geolocation.getCurrentPosition(success, error, options);
+ */
